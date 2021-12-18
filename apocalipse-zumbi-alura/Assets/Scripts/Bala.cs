6c52a1724d9da398b7539291df76d113a3b1a247
McBalbino <cecyliasb@gmail.com>
2021-12-18 14:15:46 -0300

    colocando arma e atirando

---
+++ apocalipse-zumbi-alura/Assets/Scripts/Bala.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala : MonoBehaviour
+{
+    public float Velocidade = 20;
+    // Start is called before the first frame update
+
+    // fazendo bala ir em linha reta
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().MovePosition
+            (GetComponent<Rigidbody>().position + 
+             transform.forward * Velocidade * Time.deltaTime);
+    }
+}
--- apocalipse-zumbi-alura/Assets/Scripts/ControlaInimigo.cs
+++ apocalipse-zumbi-alura/Assets/Scripts/ControlaInimigo.cs
@@ -22,9 +22,20 @@ public class ControlaInimigo : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector3 direcao = Jogador.transform.position - transform.position;
-        GetComponent<Rigidbody>().MovePosition
+        float distancia = Vector3.Distance(transform.position, Jogador.transform.position);
+
+        if (distancia > 2.5)
+        {
+            //fazendo os zumbis pararem perto do jogador
+            Vector3 direcao = Jogador.transform.position - transform.position;
+            GetComponent<Rigidbody>().MovePosition
             (GetComponent<Rigidbody>().position + 
              direcao.normalized * Velocidade * Time.deltaTime);
+            
+            //fazendo os zumbis olharem na direcao do jogador
+            Quaternion novaRotacao = Quaternion.LookRotation(direcao);
+                
+            GetComponent<Rigidbody>().MoveRotation(novaRotacao);
+        }
     }
 }
