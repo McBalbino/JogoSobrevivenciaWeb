9e0d9f326cdf9970240bc56a1d64e618f6fbf6d3
McBalbino <cecyliasb@gmail.com>
2021-12-20 14:57:25 -0300

    musica no jogo

---
+++ apocalipse-zumbi-alura/Assets/Scripts/ControlaAudio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaAudio : MonoBehaviour
+{
+    private AudioSource meuAudioSource;
+    public static AudioSource instancia;
+
+    //o awake roda antes mesmo do start
+    private void Awake()
+    {
+        meuAudioSource = GetComponent<AudioSource>();
+        instancia = meuAudioSource;
+    }
+}
