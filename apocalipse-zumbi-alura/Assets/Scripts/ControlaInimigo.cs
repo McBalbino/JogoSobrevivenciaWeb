e3b1298ddfb9b7898a2df313ebbb446b0bd19bba
McBalbino <cecyliasb@gmail.com>
2021-12-22 16:52:28 -0300

    refatorando codigo animacao

--- apocalipse-zumbi-alura/Assets/Scripts/ControlaInimigo.cs
+++ apocalipse-zumbi-alura/Assets/Scripts/ControlaInimigo.cs
@@ -7,50 +7,37 @@ using Random = UnityEngine.Random;
 public class ControlaInimigo : MonoBehaviour
 {
     public GameObject Jogador;
-    
-    private Rigidbody rigibodyInimigo;
-    private Animator animatorInimigo;
-
+    private MovimentoPersonagem movimentaInimigo;
     public float Velocidade = 5;
+    private AnimacaoPersonagem animacaoInimigo;
+    
     // Start is called before the first frame update
     void Start()
     {
         Jogador = GameObject.FindWithTag("Jogador");
-        int geraTipoZumbi = Random.Range(1, 28);
-        transform.GetChild(geraTipoZumbi).gameObject.SetActive(true);
-        rigibodyInimigo = GetComponent<Rigidbody>();
-        animatorInimigo = GetComponent<Animator>();
+        animacaoInimigo = GetComponent<AnimacaoPersonagem>();
+        movimentaInimigo = GetComponent<MovimentoPersonagem>();
+        AleatorizarZumbi();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
     private void FixedUpdate()
     {
         float distancia = Vector3.Distance(transform.position, Jogador.transform.position);
 
         //fazendo os zumbis olharem na direcao do jogador
         Vector3 direcao = Jogador.transform.position - transform.position;
-        Quaternion novaRotacao = Quaternion.LookRotation(direcao);
-                
-        GetComponent<Rigidbody>().MoveRotation(novaRotacao);
+        movimentaInimigo.Rotacionar(direcao);
+
         if (distancia > 2.5)
         {
             //fazendo os zumbis pararem perto do jogador
-            rigibodyInimigo.MovePosition
-            (rigibodyInimigo.position + 
-             direcao.normalized * Velocidade * Time.deltaTime);
-            
-            animatorInimigo.SetBool("Atacando", false);
-
+            movimentaInimigo.Movimentar(direcao, Velocidade);
+            animacaoInimigo.Atacar(false);
         }
         else
         {
             //fazendo os zumbis atacarem
-            animatorInimigo.SetBool("Atacando", true);
+            animacaoInimigo.Atacar(true);
         }
     }
     
@@ -60,5 +47,11 @@ public class ControlaInimigo : MonoBehaviour
         int dano = Random.Range(20, 25);
         //pegando uma variavel de outra classe
         Jogador.GetComponent<ControlaJogador>().TomarDano(dano);
+    }
+
+    void AleatorizarZumbi()
+    {
+        int geraTipoZumbi = Random.Range(1, 28);
+        transform.GetChild(geraTipoZumbi).gameObject.SetActive(true);
     }
 }
---
+++ apocalipse-zumbi-alura/Assets/Scripts/MovimentoPersonagem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoPersonagem : MonoBehaviour
+{
+    private Rigidbody meuRigidbody;
+
+    private void Awake ()
+    {
+        meuRigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void Movimentar(Vector3 direcao, float velocidade)
+    {
+        meuRigidbody.MovePosition
+        (meuRigidbody.position + 
+         direcao.normalized * velocidade * Time.deltaTime);
+    }
+
+    public void Rotacionar(Vector3 direcao)
+    {
+        Quaternion novaRotacao = Quaternion.LookRotation(direcao);
+        GetComponent<Rigidbody>().MoveRotation(novaRotacao);
+    }
+}
