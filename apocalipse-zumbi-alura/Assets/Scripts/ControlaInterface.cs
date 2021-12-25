16e69ef67d8e6776c4459bcf82878940db847280
McBalbino <cecyliasb@gmail.com>
2021-12-25 14:33:29 -0300

    Salvando valores na Unity

--- apocalipse-zumbi-alura/Assets/Scripts/ControlaInterface.cs
+++ apocalipse-zumbi-alura/Assets/Scripts/ControlaInterface.cs
@@ -11,6 +11,8 @@ public class ControlaInterface : MonoBehaviour
     public Slider SliderVidaJogador;
     public GameObject PainelDeGameOver;
     public Text TextoTempoDeSobrevivencia;
+    public Text TextoPontuacaoMaxima;
+    private float tempoPontuacaoSalvo;
     
     // Start is called before the first frame update
     void Start()
@@ -21,6 +23,7 @@ public class ControlaInterface : MonoBehaviour
         SliderVidaJogador.maxValue = scriptControlaJogador.statusJogador.Vida;
         AtualizarSliderVidaJogador();
         Time.timeScale = 1;
+        tempoPontuacaoSalvo = PlayerPrefs.GetFloat("PontuacaoMaxima");
 
     }
     
@@ -39,6 +42,27 @@ public class ControlaInterface : MonoBehaviour
         int segundos = (int)(Time.timeSinceLevelLoad % 60);
         TextoTempoDeSobrevivencia.text = 
             "Você sobreviveu por " + minutos + "min e " + segundos +"s";
+        
+        AjustarPontuacaoMaxima(minutos, segundos);
+    }
+
+    void AjustarPontuacaoMaxima(int min, int seg)
+    {
+        if (Time.timeSinceLevelLoad > tempoPontuacaoSalvo)
+        {
+            tempoPontuacaoSalvo = Time.timeSinceLevelLoad;
+            TextoPontuacaoMaxima.text = 
+                string.Format("Seu melhor tempo é {0}min e {1}s", min, seg);
+            PlayerPrefs.SetFloat("PontuacaoMaxima", tempoPontuacaoSalvo);
+        }
+
+        if (TextoPontuacaoMaxima.text == "")
+        {
+            min = (int) tempoPontuacaoSalvo / 60;
+            seg = (int) tempoPontuacaoSalvo % 60;
+            TextoPontuacaoMaxima.text = 
+                string.Format("Seu melhor tempo é {0}min e {1}s", min, seg);
+        }
     }
 
     public void Reiniciar()
