d79010d2f33fcc956be970b144113174b0119e68
McBalbino <cecyliasb@gmail.com>
2022-01-02 18:19:26 -0300

    particulas

--- apocalipse-zumbi-alura/Assets/Scripts/ControlaChefe.cs
+++ apocalipse-zumbi-alura/Assets/Scripts/ControlaChefe.cs
@@ -15,6 +15,7 @@ public class ControlaChefe : MonoBehaviour, IMatavel
     public Slider sliderVidaChefe;
     public Image ImageSlider;
     public Color CorDaVidaMaxima, CorDaVidaMinima;
+    public GameObject ParticulaSangueZumbi;
 
     private void Start()
     {
@@ -63,6 +64,11 @@ public class ControlaChefe : MonoBehaviour, IMatavel
         {
             Morrer();
         }
+    }
+    
+    public void ParticulaSangue(Vector3 posicao, Quaternion rotacao)
+    {
+        Instantiate(ParticulaSangueZumbi, posicao, rotacao);
     }
 
     public void Morrer()
